5d5ea462ccbe99e309cdc08baf41e60e58820a0b
Brayden Phillips <bphillips@playvox.com>
2022-07-14 11:47:48 +1000

    Added ui application

---
+++ apps/ui/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ChakraProvider } from '@chakra-ui/react';
+import './styles.css';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Head>
+        <title>Welcome to ui!</title>
+      </Head>
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
+}
+
+export default CustomApp;
