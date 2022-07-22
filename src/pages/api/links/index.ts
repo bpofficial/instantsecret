60365b51549e40f244edc7cf2d8d64de294bbc99
Brayden Phillips <bphillips@playvox.com>
2022-07-22 18:12:41 +1000

    wip

--- src/pages/api/links/index.ts
+++ src/pages/api/links/index.ts
@@ -2,6 +2,7 @@ import { API } from "aws-amplify";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function linksReq(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req)
     if (req.method === "POST") {
         const {
             value,
@@ -20,8 +21,10 @@ export default function linksReq(req: NextApiRequest, res: NextApiResponse) {
                 recipients,
             },
         }).then((result: any) => {
+            console.log(result)
             res.redirect(`/links/${result.linkId}`);
         }).catch((err: any) => {
+            console.log(err)
             res.redirect(`/links?error=${(err.message || err)}&error_code=500`);
         });
     } else {
