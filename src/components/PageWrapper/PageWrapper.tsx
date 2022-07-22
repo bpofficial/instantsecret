c7494a44e572ff76369c654396e2494afe06d1bf
Brayden Phillips <bphillips@playvox.com>
2022-07-22 11:00:06 +1000

    wip

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -1,19 +1,15 @@
 import { Flex, useMediaQuery } from '@chakra-ui/react';
 
 export const PageWrapper = ({ children }: any) => {
-    const [isLargerThan767, isLargerThan1199] = useMediaQuery(
-        ['(min-width: 767px)', '(min-width: 1199px)'],
-        { fallback: [false, true], ssr: true }
-    );
-
     return (
         <Flex
-            mt={isLargerThan1199 ? '12' : undefined}
+            mt={{ base: '12', sm: '0' }}
             w="100%"
-            px={isLargerThan767 ? '24' : '6'}
-            py={isLargerThan767 ? '12' : '4'}
-            direction={isLargerThan1199 ? 'row' : 'column-reverse'}
-            justifyContent="space-between"
+            px={{ base: 24, sm: 6 }}
+            py={{ base: 12, sm: 4 }}
+            direction={{ base: 'row', sm: 'column-reverse' }}
+            justifyContent={'space-between'}
+            alignItems="center"
         >
             {children}
         </Flex>
