679c4a4ffc4068fe891b7fd40b83330f77dd8810
Brayden <bpofficial@icloud.com>
2022-07-26 18:30:52 +1000

    Fixed env

--- src/aws.ts
+++ src/aws.ts
@@ -2,10 +2,10 @@ import AWS from "aws-sdk";
 
 AWS.config.update({
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.ISR_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.ISR_AWS_SECRET_ACCESS_KEY!,
     },
-    region: process.env.AWS_REGION,
+    region: process.env.ISR_AWS_REGION,
     signatureVersion: "v4",
 });
 
@@ -15,7 +15,7 @@ export const getDynamodb = () => {
     const dynamodb = new AwsService.DynamoDB.DocumentClient();
 
     let env = "default";
-    let tableName = process.env.DYNAMO_TABLE_NAME!;
+    let tableName = process.env.TABLE_NAME!;
     if (process.env.ENV && process.env.ENV !== "NONE") {
         env = process.env.ENV;
         tableName = tableName + "-" + process.env.ENV;
