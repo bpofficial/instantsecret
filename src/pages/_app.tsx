7dc09919660e38c1729aa16aef65220f39d43b5c
Brayden <bpofficial@icloud.com>
2022-07-29 14:06:50 +1000

    Added GTAG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -85,6 +85,29 @@ export default function InstantSecret({ Component, pageProps }: AppProps) {
                     />
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#ffffff" />
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    {process.env.ENV === "production" ? (
+                        <>
+                            <script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+                                }}
+                            />
+                        </>
+                    ) : (
+                        <></>
+                    )}
                 </Head>
                 <CounterProvider>
                     <chakra.main
