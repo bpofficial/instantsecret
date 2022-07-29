927378a4eb61ff703e8591f89b71339ee1f30149
Brayden <bpofficial@icloud.com>
2022-07-29 14:02:52 +1000

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,7 +45,7 @@ export default function InstantSecret({ Component, pageProps }: AppProps) {
                     />
                     <meta name="robots" content="index, follow" />
                     <meta
-                        http-equiv="Content-Type"
+                        httpEquiv="Content-Type"
                         content="text/html; charset=utf-8"
                     />
                     <meta name="language" content="English" />
