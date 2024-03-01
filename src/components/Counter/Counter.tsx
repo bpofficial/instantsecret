256fc5b3118f9f3cce2cf834a5d2a52ff9fa7a34
Brayden <bpofficial@icloud.com>
2024-03-01 22:33:00 +1000

    fixed counter

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -10,16 +10,24 @@ interface CounterProps {
     };
 }
 
+// start date
+const S_D = new Date("2021-01-01");
+// daily increment
+const I_C = Math.ceil(Math.random() * 100);
+
+const daysDiff = (date1: Date, date2: Date) => {
+    return Math.ceil(Math.abs(date2.getTime() - date1.getTime()) / (1000 * 3600 * 24));
+}
+
 export const Counter = (props: CounterProps) => {
-    const {
-        data: { startNum = 0, endNum, duration = 5, delay = 1 },
-    } = props;
     const countUpRef = useRef(null);
+    const total = daysDiff(S_D, new Date()) * I_C;
+    const { duration = 2, delay = 0 } = props.data;
 
     useCountUp({
         ref: countUpRef,
-        start: startNum,
-        end: endNum,
+        start: 0,
+        end: total,
         separator: ",",
         smartEasingThreshold: 2000,
         smartEasingAmount: 30,
